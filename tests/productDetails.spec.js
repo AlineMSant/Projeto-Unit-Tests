17fab0d451636cf8c1d64d73f96375a24cc8ee49
AlineMSant <alinems4120@gmail.com>
2022-11-25 13:23:54 -0300

    requisito 6 testa se array contem 2 itens

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -42,6 +42,12 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
 
     expect(testArray).toBe(true)
     // Teste se o array retornado pela função contém dois itens dentro.
+    let testLength;
+    if (resultado.length === 2){
+      testLength = true;
+    }
+
+    expect(testLength).toBe(true);
     // Teste se os dois itens dentro do array retornado pela função são objetos.
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
     // Teste se os dois productIds terminam com 123.
